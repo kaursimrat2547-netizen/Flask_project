51542f921e1f06ca65bf7cd2f7466d01245d8e05
SSCXIT08 <kaursimrat2547@gmail.com>
2026-01-01 10:06:21 +0530

    First commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home(): 
-    return "Hello! This is a simple flask app running inside docker."
+    return "Hello! This is a simple flask app running inside docker.SIMRAT."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
